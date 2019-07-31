29480003c53cd2c58490dd24681c2ad5308fd82d
AmmarAlkhooly98 <ammaralkhooly1@gmail.com>
2019-07-31 21:59:16 +0300

    added template successfully

---
+++ front-end/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('build'));
+app.use(bodyParser.json());
+
+
+const server = app.listen(port, () => {
+	console.log(`app listening on port ${port}!`);
+});
