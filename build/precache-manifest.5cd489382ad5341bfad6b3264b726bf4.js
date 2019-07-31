c857a9a8742f7c15b5f65e9131e0967b5754313b
AmmarAlkhooly98 <ammaralkhooly1@gmail.com>
2019-07-31 22:21:02 +0300

    track package.json

---
+++ build/precache-manifest.5cd489382ad5341bfad6b3264b726bf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8243bf7904f2049a3919af1ee60b072",
+    "url": "/index.html"
+  },
+  {
+    "revision": "011b6b75f0d1b479bc73",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "011b6b75f0d1b479bc73",
+    "url": "/static/js/main.67044ccd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
