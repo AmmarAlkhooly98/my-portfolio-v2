9ea2d769afc626dcd6fc08eb72830843f8dcb37f
AmmarAlkhooly98 <ammaralkhooly1@gmail.com>
2019-08-03 22:46:21 +0300

    portfolio sections completed  email api left

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ const request = mailjet
 				"Email": "ammaralkhooly1@gmail.com",
 				"Name": "Ammar"
 			}],
-			"Subject": "Greetings from Mailjet.",
-			"TextPart": "My first Mailjet email",
+			"Subject": "",
+			"TextPart": "",
 			"HTMLPart": "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!",
 			"CustomID": "AppGettingStartedTest"
 		}]
