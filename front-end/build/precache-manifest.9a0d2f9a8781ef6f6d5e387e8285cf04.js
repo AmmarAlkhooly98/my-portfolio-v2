a194ffbac8f5edf3b756806a21c668163f68f0cf
AmmarAlkhooly98 <ammaralkhooly1@gmail.com>
2019-11-22 21:10:35 +0200

    make it better

---
+++ front-end/build/precache-manifest.9a0d2f9a8781ef6f6d5e387e8285cf04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c158e7116890c1d07a82884881a647f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e96004f581254b57aa70",
+    "url": "/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "e96004f581254b57aa70",
+    "url": "/static/js/main.eb79b116.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- front-end/server.js
+++ front-end/server.js
@@ -4,11 +4,11 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(
-	bodyParser.urlencoded({
-		extended: true
-	})
-);
+// app.use(
+// 	bodyParser.urlencoded({
+// 		extended: true
+// 	})
+// );
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
