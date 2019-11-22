6ffca4536e5fee2418528434f3ad666a7295ee99
AmmarAlkhooly98 <ammaralkhooly1@gmail.com>
2019-11-22 19:37:16 +0200

    my portfolio update 2

--- front-end/server.js
+++ front-end/server.js
@@ -5,10 +5,42 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('build'));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(express.static('public'));
 app.use(bodyParser.json());
 
+
+const mailjet = require('node-mailjet')
+	.connect('cd18c8e79bbe99f95febcda2a50bbdb4', '980395f4618977e5142ada18c45e49a3')
+const request = mailjet
+	.post("send", {
+		'version': 'v3.1'
+	})
+	.request({
+		"Messages": [{
+			"From": {
+				"Email": "",
+				"Name": ""
+			},
+			"To": [{
+				"Email": "ammaralkhooly1@gmail.com",
+				"Name": "Ammar"
+			}],
+			"Subject": "",
+			"TextPart": "",
+			"HTMLPart": "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!",
+			"CustomID": "AppGettingStartedTest"
+		}]
+	})
+request
+	.then((result) => {
+		console.log(result.body)
+	})
+	.catch((err) => {
+		console.log(err.statusCode)
+	})
 
 const server = app.listen(port, () => {
 	console.log(`app listening on port ${port}!`);
