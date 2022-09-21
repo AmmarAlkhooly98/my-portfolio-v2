f7e2f28705e5124827df52131f92bb28ad08f5f1
Ammar Alkhooly <ammaralkhooly1@gmail.com>
2022-09-21 16:11:49 +0300

    merged changes with upstream - improved client submit email event handler logic

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,20 @@
-$("#submitEmail").click(() => {
+$("#submitEmail").click((e) => {
+  e.preventDefault();
+  e.stopImmediatePropagation();
   const API_ENDPOINT = "/api/v1/mailer";
   const userFeedback = $("#contact-feedback");
-  let username = $("#contact-name").val();
-  let email = $("#contact-email").val();
-  let subject = $("#contact-subject").val();
-  let message = $("#contact-message").val();
+  const username = $("#contact-name").val();
+  const email = $("#contact-email").val();
+  const subject = $("#contact-subject").val();
+  const message = $("#contact-message").val();
 
   if (!username || !email || !subject || !message) {
-    userFeedback.fadeIn("fast");
     userFeedback.css("color", "orange");
-    userFeedback.html("Please fill in all fields.");
-    userFeedback.fadeOut(10000);
+    userFeedback
+      .html("Please fill in all fields.")
+      .show()
+      .delay(4000)
+      .fadeOut("slow");
   } else {
     axios
       .post(API_ENDPOINT, {
@@ -20,19 +24,19 @@ $("#submitEmail").click(() => {
         message,
       })
       .then((res) => {
-        userFeedback.fadeIn("fast");
         userFeedback.css("color", "blue");
-        userFeedback.html(res.data);
-        userFeedback.fadeOut(10000);
+        userFeedback.html(res.data.message).show().delay(5000).fadeOut("slow");
         $(":input", "#contact-form")
           .not(":button, :submit, :reset, :hidden")
           .val("");
       })
       .catch((e) => {
-        userFeedback.fadeIn("fast");
         userFeedback.css("color", "#FFC300");
-        userFeedback.html(e.response.data);
-        userFeedback.fadeOut(10000);
+        userFeedback
+          .html(e.response.data.message)
+          .show()
+          .delay(5000)
+          .fadeOut("slow");
       });
   }
 });
